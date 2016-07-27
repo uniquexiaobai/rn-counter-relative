354a7b613fd3ff94c0ff00f40d4717fa8fa2d061
uniquexiaobai <unique_xiaobai@163.com>
2016-07-27 23:09:50 +0800

    ios running ok

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,9 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * 亲戚称谓计算器
+ * https://github.com/uniquexiaobai/rn-counter-relative
  */
 
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { AppRegistry } from 'react-native';
+import Main from './components/main';
 
-class counterRelativeAndroid extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('counterRelativeAndroid', () => counterRelativeAndroid);
+AppRegistry.registerComponent('counterRelativeAndroid', () => Main);
